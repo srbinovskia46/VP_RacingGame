e5ea41ba24e4c2b7743f854ba58c43917188245f
Vasil <vsilt2002@gmail.com>
2023-06-12 04:11:58 +0200

    Give the coins more style

--- RacingGame/Aicoins.cs
+++ RacingGame/Aicoins.cs
@@ -4,6 +4,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
 
 namespace RacingGame
 {
@@ -15,6 +16,7 @@ namespace RacingGame
         public int Height { get; set; }
         public int Speed { get; set; }
         public Color Color { get; set; }
+        public Font font { get; set; }
         public Point Location { get; internal set; }
 
         public Aicoins(int x, int y, int width, int height, int speed, Color color)
@@ -25,15 +27,20 @@ namespace RacingGame
             Height = height;
             Speed = speed;
             Color = color;
+            font=new Font("Arial",13);
         }
 
         public void Draw(Graphics g)
         {
             Brush brush = new SolidBrush(Color);
+            Brush fb = new SolidBrush(Color.Orange);
             g.FillEllipse(brush, X, Y, Width, Height);
             Pen pen = new Pen(Color.Orange, 3);
             g.DrawEllipse(pen, X, Y, Width, Height);
+            g.DrawString(string.Format("C"), font, fb, X+3, Y+3);
             pen.Dispose();
+            fb.Dispose();
+            brush.Dispose();
         }
 
         public void Update()
